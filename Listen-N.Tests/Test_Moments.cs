cab3c9bad4b032e5c64c91447536d97ea4842a5c
natalieclauren-a11y <natalieclauren@gmail.com>
2025-12-02 16:11:51 -0700

    Fix BaseBinAccumulator reflection arguments

--- Listen-N.Tests/Test_Moments.cs
+++ Listen-N.Tests/Test_Moments.cs
@@ -107,7 +107,7 @@ namespace AdaptiveWindowTests
                 accumulatorType,
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
                 binder: null,
-                args: new object[] { gateUs, windowSec },
+                args: new object[] { gateUs, windowSec, (int?)null },
                 culture: null);
 
             var addMethod = accumulatorType.GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
