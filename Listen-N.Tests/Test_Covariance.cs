a74f126187ce96fa0528d61cf13daf8482760c24
natalieclauren-a11y <natalieclauren@gmail.com>
2025-12-02 18:15:54 -0700

    Update Test_Covariance.cs

--- Listen-N.Tests/Test_Covariance.cs
+++ Listen-N.Tests/Test_Covariance.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Listen_N;
@@ -78,22 +78,23 @@ namespace AdaptiveWindowTests
             int gateUs = 1;
             double windowSec = Math.Max(1, gates.Count) * gateUs / 1e6;
 
-            var accumulatorType = typeof(AdaptiveWindowEngine).GetNestedType("BaseBinAccumulator", BindingFlags.NonPublic)
-                ?? throw new InvalidOperationException("Unable to locate BaseBinAccumulator via reflection.");
+            var accumulatorType = typeof(AdaptiveWindowEngine).GetNestedType("BaseBinAccumulator", BindingFlags.NonPublic);
+            if (accumulatorType == null)
+                throw new InvalidOperationException("Unable to locate BaseBinAccumulator via reflection.");
 
             object? accumulator = Activator.CreateInstance(
                 accumulatorType,
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
-                binder: null!,
-                args: new object[] { gateUs, windowSec, (int?)null },
+                binder: null,
+                args: new object[] { gateUs, windowSec },
                 culture: null);
             if (accumulator == null)
                 throw new InvalidOperationException("Failed to instantiate BaseBinAccumulator.");
 
-            var addMethod = accumulatorType.GetMethod("Add", BindingFlags.Instance | BindingFlags.Public)
-                ?? throw new InvalidOperationException("Missing expected Add method on BaseBinAccumulator.");
-            var computeMethod = accumulatorType.GetMethod("ComputeMoments", BindingFlags.Instance | BindingFlags.Public)
-                ?? throw new InvalidOperationException("Missing expected ComputeMoments method on BaseBinAccumulator.");
+            var addMethod = accumulatorType.GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
+            var computeMethod = accumulatorType.GetMethod("ComputeMoments", BindingFlags.Instance | BindingFlags.Public);
+            if (addMethod == null || computeMethod == null)
+                throw new InvalidOperationException("Missing expected BaseBinAccumulator methods.");
 
             for (int i = 0; i < gates.Count; i++)
             {
