56c780872eeae78b727cff7623c011ff69e0b949
natalieclauren-a11y <natalieclauren@gmail.com>
2026-01-05 14:25:00 -0800

    Use area series for outcome distribution plot

--- Localization.Train/OutcomeDistributionFigureWriter.cs
+++ Localization.Train/OutcomeDistributionFigureWriter.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
@@ -16,20 +17,26 @@ public static class OutcomeDistributionFigureWriter
         string title,
         string subtitle)
     {
-        var model = new PlotModel { Title = title };
-
-        model.Subtitle = subtitle;
-
-        var categoryAxis = new CategoryAxis
+        var model = new PlotModel
         {
-            Position = AxisPosition.Bottom,
-            ItemsSource = new[] { "Single", "Dual", "Centroid", "Refuse" },
-            MajorGridlineStyle = LineStyle.Solid,
-            MinorGridlineStyle = LineStyle.None,
-            GapWidth = 0.2
+            Title = title,
+            Subtitle = subtitle + " | 1=Single 2=Dual 3=Centroid 4=Refuse",
         };
 
-        model.Axes.Add(categoryAxis);
+        var xAxis = new LinearAxis
+        {
+            Position = AxisPosition.Bottom,
+            Minimum = 0.5,
+            Maximum = 4.5,
+            MajorStep = 1,
+            MajorGridlineStyle = LineStyle.Solid,
+            MinorGridlineStyle = LineStyle.None,
+            MajorTickSize = 0,
+            MinorTickSize = 0,
+            LabelFormatter = _ => string.Empty,
+        };
+
+        model.Axes.Add(xAxis);
 
         var valueAxis = new LinearAxis
         {
@@ -38,27 +45,40 @@ public static class OutcomeDistributionFigureWriter
             Maximum = 1,
             MajorGridlineStyle = LineStyle.Solid,
             MinorGridlineStyle = LineStyle.None,
-            Title = "Fraction of windows"
+            Title = "Fraction of windows",
         };
 
         model.Axes.Add(valueAxis);
 
-        var series = new ColumnSeries
+        model.Series.Add(Bar(1, singleFrac, 0.8, "Single", OxyColor.FromAColor(140, OxyColors.SteelBlue), OxyColors.SteelBlue));
+        model.Series.Add(Bar(2, dualFrac, 0.8, "Dual", OxyColor.FromAColor(140, OxyColors.ForestGreen), OxyColors.ForestGreen));
+        model.Series.Add(Bar(3, centroidFrac, 0.8, "Centroid", OxyColor.FromAColor(140, OxyColors.Goldenrod), OxyColors.Goldenrod));
+        model.Series.Add(Bar(4, refuseFrac, 0.8, "Refuse", OxyColor.FromAColor(140, OxyColors.IndianRed), OxyColors.IndianRed));
+
+        using var stream = File.Create(outputPath);
+        var pngExporter = new PngExporter { Width = 900, Height = 600 };
+        pngExporter.Export(model, stream);
+    }
+
+    private static AreaSeries Bar(double xCenter, double height, double width, string title, OxyColor fill, OxyColor stroke)
+    {
+        double x0 = xCenter - width / 2.0;
+        double x1 = xCenter + width / 2.0;
+
+        var series = new AreaSeries
         {
-            StrokeThickness = 1,
-            FillColor = OxyColors.SteelBlue,
-            StrokeColor = OxyColors.Black,
-            ColumnWidth = 0.5
+            Title = title,
+            Color = stroke,
+            Fill = fill,
+            StrokeThickness = 1.5,
         };
 
-        series.Items.Add(new ColumnItem(singleFrac));
-        series.Items.Add(new ColumnItem(dualFrac));
-        series.Items.Add(new ColumnItem(centroidFrac));
-        series.Items.Add(new ColumnItem(refuseFrac));
+        series.Points.Add(new DataPoint(x0, height));
+        series.Points.Add(new DataPoint(x1, height));
 
-        model.Series.Add(series);
+        series.Points2.Add(new DataPoint(x0, 0));
+        series.Points2.Add(new DataPoint(x1, 0));
 
-        var pngExporter = new PngExporter { Width = 900, Height = 600, Background = OxyColors.White };
-        pngExporter.ExportToFile(model, outputPath);
+        return series;
     }
 }
